1d4ffd16e793d8c43ce715b5bbebf8c015c206e4
Raegrand Archamadeus <raegrand@gmail.com>
2026-04-20 08:35:09 +0700

    V1.1

--- soal1.c
+++ soal1.c
@@ -1,5 +1,35 @@
-#include <stdio.h>
+ //   Modul               : 02 – Data Manipulation and External Files
+ //   Hari dan Tanggal    : Senin, 20 April 2026
+ //   Nama (NIM)          : Raegrand Archamadeus (13224014)
+ //   Nama File           : soal1.c
+ //   Deskripsi           : 
 
-int main(void) {
+# include <stdio.h>
+
+
+
+int main(void){
+    int n, max = 0;
+    scanf("%d", &n);
+    int sense[n+1];
+    for(int i = 0; i<n; i++){
+        scanf("%d", &sense[i]);
+    }
+    for(int i = n-1; i >= 0; i--){
+        if(sense[i] == -1){
+            if(i == 0){sense[i] = sense[i+1];}
+            else{
+                sense[i] = (sense[i-1] == -1)? sense[i+1] : (sense[i+1]+sense[i-1])/2;
+            }
+        }
+    }
+
+    printf("RECOCVERED");
+    for(int i = 0; i<n; i++){
+        printf(" %d", sense[i]);
+        max += sense[i];
+    }
+    printf("\nMAX_SUM %d", max);
+
     return 0;
 }
